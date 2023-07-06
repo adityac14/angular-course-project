ac4070dcd3c2419e6e5796dc9bb48579bf18ecc8
Aditya <adityachatto14@gmail.com>
2023-07-06 18:39:47 -0400

    Created Ingredient Model and created initial shopping list view

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,5 @@
+// TypeScript alternative way to declare a class by making the parameters in the consturctor public
+export class Ingredient {
+    constructor(public name: string, public amount: number)
+    {}
+}
