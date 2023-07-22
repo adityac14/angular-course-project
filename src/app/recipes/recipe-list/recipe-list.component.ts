74a7283f4ccffb07d18ab8babf2b23b72ba6134b
Aditya <adityachatto14@gmail.com>
2023-07-21 21:11:59 -0400

    Off-loaded Selection of Recipes into RecipeService rather than in multiple components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.services';
 
@@ -8,8 +8,6 @@ import { RecipeService } from '../recipe.services';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-
   recipes : Recipe[]
 
   constructor(private recipeService: RecipeService) {}
@@ -18,7 +16,4 @@ export class RecipeListComponent implements OnInit {
     this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe){
-    this.recipeWasSelected.emit(recipe);
-  }
 }
--- src/app/recipes/recipe.services.ts
+++ src/app/recipes/recipe.services.ts
@@ -1,6 +1,8 @@
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
     
     private recipes : Recipe[] = [
         new Recipe('A Test Recipe', ' This is simply a test', 'https://www.simplyrecipes.com/thmb/KRw_r32s4gQeOX-d07NWY1OlOFk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg'),
@@ -10,7 +12,6 @@ export class RecipeService {
       getRecipes(){
         // Returns a copy of the recipes Array and not the original reference that is stored in the service
         return this.recipes.slice();
-
       }
 
 }
