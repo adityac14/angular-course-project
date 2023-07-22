fc04b8ee192204d1bf5a31b08f436cc145093932
Aditya <adityachatto14@gmail.com>
2023-07-21 21:38:20 -0400

    Moved ShoppingList Component Logic into ShoppingList Service

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.services';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -12,15 +13,13 @@ export class ShoppingEditComponent {
 
   @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef
 
-  @Output() ingredientAdded = new EventEmitter<Ingredient>()
+  constructor(private slService: ShoppingListService) {}
 
   onAddItem(){
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount)
-
-    this.ingredientAdded.emit(newIngredient)
-
+    this.slService.addIngredient(newIngredient)
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,20 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.services';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
-  ];
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[]
 
-  onIngredientAdded(ingredient: Ingredient){
-    this.ingredients.push(ingredient)
+  constructor(private slService: ShoppingListService) {}
 
+  // All initalizations should be declared in ngOnInit()
+  ngOnInit(): void {
+    this.ingredients = this.slService.getIngredients()
+    this.slService.ingredientsChanged.subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients
+      }
+    );
   }
-
 }
